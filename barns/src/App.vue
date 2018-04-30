770266e98c5e6f455cc26e9752267895c2954b10
weige <wgqin1@e.ntu.edu.sg>
2018-04-30 10:04:48 +0800

    add all reference repo

---
+++ barns/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+        <app-sidebar :messages="msg"></app-sidebar>
+    </div>
+</template>
+
+<script>
+  import Sidebar from './Sidebar.vue';
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components: {
+    appSidebar: Sidebar
+  }
+}
+</script>
+
+<style>
+
+</style>
